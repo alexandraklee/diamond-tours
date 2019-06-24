fc302fb72fb27fd27d854014eacee8bceaaccf0f
Alexandra <aklhm.x@gmail.com>
2019-06-24 16:14:56 +0800

    gallery fixed, tour pages added

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,26 @@ $(document).ready(function () {
   })
 });
 
+
+
+// This is the pop-up gallery. This code is copied directly from the magnific website, just changing for the class on the gallery. 
+
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title');
+			}
+		}
+	});
+});
