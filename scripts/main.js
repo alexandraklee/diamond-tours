84107fed9bc17a005cc796865b396087b24316a8
Alexandra <aklhm.x@gmail.com>
2019-06-24 14:01:48 +0800

    hamburger nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+  $("#hamburger").click(function(){
+    $('.mobile-nav').slideToggle();
+  });
+
+
+
   $('.nav-button').click(function() {
     $('.nav').toggleClass('mobile-nav-active');
   })
